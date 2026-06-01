796ddee27416c4203f1b67ab52eab727fa0eff5b
Leonard Werder <leonard.werder@gmail.com>
2026-06-01 14:21:15 +0200

    Fetching location

---
+++ MovieWeather/src/components/FetchLocation.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const latitude = ref<number | null>(null)
+const longitude = ref<number | null>(null)
+
+function getLocation() {
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            latitude.value = position.coords.latitude
+            longitude.value = position.coords.longitude
+
+            console.log("Latitude:", position.coords.latitude)
+            console.log("Longitude:", position.coords.longitude)
+        },
+    );
+}
+</script>
+
+<template>
+  <div>
+    
+    <button @click="getLocation">Get My Location</button>
+
+    <div v-if="latitude && longitude">
+      <p>Latitude: {{ latitude }}</p>
+      <p>Longitude: {{ longitude }}</p>
+    </div>
+
+  </div>
+</template>
