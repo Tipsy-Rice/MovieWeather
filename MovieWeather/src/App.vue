8ffc4f77748caa9ce214aea7d418f7c8e47cd3ba
Leonard Werder <leonard.werder@gmail.com>
2026-06-03 22:44:30 +0200

    Fetching weather and location on button

--- MovieWeather/src/App.vue
+++ MovieWeather/src/App.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts">
-import FetchLocation from './components/FetchLocation.vue';
+<script setup>
+import { ref } from 'vue'
+import FetchLocation from './components/FetchLocation.vue'
+import FetchWeather from './components/FetchWeather.vue'
 
+// State for coordinates and reference to FetchLocation component
+const coords = ref(null)
+const locationRef = ref(null)
+
+// Handle location data sent from FetchLocation component
+function handleLocated(newCoords) {
+  coords.value = newCoords
+}
+
+// Parent component (App.vue) will pass coords to FetchWeather component which will fetch weather data
+function requestLocation() {
+  locationRef.value?.getLocation?.()
+}
 </script>
 
 <template>
   <div>
-    <FetchLocation></FetchLocation>
+    <!-- Child components -->
+    <!-- FetchLocation will get coords and send them to parent(App.vue) -->
+    <FetchLocation ref="locationRef" @located="handleLocated" />
+    <!-- Pass coords to FetchWeather component which will fetch weather -->
+    <FetchWeather :coords="coords" @request-location="requestLocation" />
   </div>
 </template>
 
--- MovieWeather/src/components/FetchLocation.vue
+++ MovieWeather/src/components/FetchLocation.vue
@@ -1,32 +1,36 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
 
-const latitude = ref<number | null>(null)
-const longitude = ref<number | null>(null)
+// Props from parent(App.vue)
+const emit = defineEmits(['located'])
 
+// State for location data and error message
+const latitude = ref(null)
+const longitude = ref(null)
+const error = ref(null)
+
+// Function to get user's location using Geolocation API
 function getLocation() {
+  error.value = null
 
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            latitude.value = position.coords.latitude
-            longitude.value = position.coords.longitude
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      latitude.value = position.coords.latitude
+      longitude.value = position.coords.longitude
 
-            console.log("Latitude:", position.coords.latitude)
-            console.log("Longitude:", position.coords.longitude)
-        },
-    );
+      emit('located', { latitude: latitude.value, longitude: longitude.value })
+    },
+    () => {
+      error.value = 'Could not get your location.'
+    }
+  )
 }
+// Expose getLocation method to parent component (App.vue) so it can be called when FetchWeather component requests location data
+defineExpose({ getLocation })
 </script>
 
 <template>
   <div>
-    
-    <button @click="getLocation">Get My Location</button>
-
-    <div v-if="latitude && longitude">
-      <p>Latitude: {{ latitude }}</p>
-      <p>Longitude: {{ longitude }}</p>
-    </div>
-
+    <p v-if="error">{{ error }}</p>
   </div>
 </template>
