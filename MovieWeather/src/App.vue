028ca1ecaf3d1ec134bef9984cbd6137886bd7f0
GoblinBuilds <ohrocky@live.com>
2026-06-07 20:54:17 +0200

    Removed unused files and fixed HomeView

--- MovieWeather/src/App.vue
+++ MovieWeather/src/App.vue
@@ -1,7 +1,20 @@
-
+<script setup lang="ts">
+import { RouterView, RouterLink } from 'vue-router'
 </script>
- 
-<template>
 
-}
-</style>
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md" style="background-color: #008979;">
+      <div class="container-fluid px-4">
+        <RouterLink to="/" class="navbar-brand text-white fw-bold fs-4">MovieWeather</RouterLink>
+        <div class="d-flex gap-4">
+          <RouterLink to="/" class="text-white text-decoration-none">Home</RouterLink>
+          <RouterLink to="/watchlist" class="text-white text-decoration-none">My Watchlist</RouterLink>
+        </div>
+      </div>
+    </nav>
+    <main class="container-fluid px-4 py-4">
+      <RouterView />
+    </main>
+  </div>
+</template>
